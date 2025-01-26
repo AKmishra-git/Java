f3981d769b72e44f521ad618dc782422e6f7da90
Ayush Mishra <akumarmishra652@gmail.com>
2025-01-26 11:03:13 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the no of rows you want: ");
+        int n = s.nextInt();
+
+        for(int i=1;i<=n;i++){
+            System.out.print("*"); //work
+
+            System.out.println(); 
+
+        }
+    }
+    
+}
